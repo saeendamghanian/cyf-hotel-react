526237f3a38629f4c54effd4bffc4325c569cf5f
Saeen-Damghanian <saeen.damghanian@gmail.com>
2019-12-04 17:50:56 +0100

    Updates

---
+++ precache-manifest.87214ef097bbd608c2ff3c9941b51ccc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1644cbe58546c79cb9f0115affef2d8",
+    "url": "/cyf-hotel-react/index.html"
+  },
+  {
+    "revision": "661a60d7ffde1b1a6077",
+    "url": "/cyf-hotel-react/static/css/main.2b0535e7.chunk.css"
+  },
+  {
+    "revision": "3b3822a746a5a1629310",
+    "url": "/cyf-hotel-react/static/js/2.9a8965b6.chunk.js"
+  },
+  {
+    "revision": "661a60d7ffde1b1a6077",
+    "url": "/cyf-hotel-react/static/js/main.2cc8ee6b.chunk.js"
+  },
+  {
+    "revision": "34311b7f62da8b8a0d63",
+    "url": "/cyf-hotel-react/static/js/runtime~main.dc6ae9fc.js"
+  }
+]);
